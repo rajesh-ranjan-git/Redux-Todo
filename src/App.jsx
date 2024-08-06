79686f785d599703b5ccccf80e8e1d581311a874
Rajesh <rajeshranjan8271@gmail.com>
2024-08-06 08:52:56 +0530

    Tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="bg-slate-200 w-screen h-screen">
+      <h1 className="text-purple-950 ">Redux Todo</h1>
+    </div>
+  );
 }
 
 export default App;
